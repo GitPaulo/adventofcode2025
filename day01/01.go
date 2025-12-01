379ed6a968edc09e6bd58ae57647a5841a95db77
Paulo Santos <GitPaulo@users.noreply.github.com>
2025-12-01 19:06:51 +0000

    part2 solved

--- day01/01.go
+++ day01/01.go
@@ -45,6 +45,35 @@ func solve(lines []string) (int, int) {
 	part1 = password
 
 	// Part 2
+	dial = 50
+	password = 0
+	for _, line := range lines {
+		direction := line[0] // L or R
+		value, _ := strconv.Atoi(line[1:]) // number of steps
+		remainder := value % 100
+
+		// count full rotations
+		password += value / 100
+
+		if direction == 'L' {
+			// count partial rotation if we cross/land on 0
+			// skip if dial is already at 0
+			if dial > 0 && dial <= remainder {
+				password++
+			}
+			dial -= value
+		} else if direction == 'R' {
+			// count partial rotation if we wrap past 99 to 0
+			if dial + remainder > 99 {
+				password++
+			}
+			dial += value
+		}
+
+		// wrap to 0-99
+		dial = ((dial % 100) + 100) % 100
+	}
+	part2 = password
 
 	return part1, part2
 }
